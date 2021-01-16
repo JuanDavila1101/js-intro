1792c4ca40a4653ee2916091bf026f1e5bb44800
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-16 12:01:01 -0600

    This is intro to functions in js

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
 console.log("js-intro for JID ");
+
+
+// function sayHello() {
+//     console.log('Hello World!');  
+// }
+// const sayHello = function() {
+//     console.log('Hello World!');
+// }
+// const sayHello = () => {
+//     console.log('Hello World!');
+// }
+// sayHello();
+
+
+// function logsHello(name) {
+//     console.log(`Hello, ${name}`);
+// }
+// logsHello('Juan');
+
+
+// const sum = 55;
+// const addTwoNumbers = (num1, num2) => {
+//     const sum = num1 + num2;
+//     console.log(sum);
+//     return sum;
+// }
+// console.log(sum);
+// addTwoNumbers(2, 1);
+// console.log(addTwoNumbers(2, 1));
+// console.log(sum);
+
+let sum = 55;
+const addTwoNumbers = (num1, num2) => {
+    sum = num1 + num2;
+    //  console.log(sum);
+    return sum;
+}
+console.log(sum);
+addTwoNumbers(2, 1);
+console.log(addTwoNumbers(2, 1));
+console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//END
