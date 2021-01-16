242d938b77fbacd82880034f60f700db7e040c27
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-16 13:09:34 -0600

    this is conditional logic control flow

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("js-intro for JID ");
+// console.log("js-intro for JID ");
+
+// function thisIsTrue(arg1, arg2) {
+//     if (arg1 === 'nice' || arg2 === 'Nice') {
+//         console.log(`You are ${arg1}`);
+//     } else {
+//         console.log('false');
+//     }
+// }
+
+// function thisIsTrue(arg1, arg2, arg3) {
+//     if (arg1 === 'nice' || arg2 === 'Nice') {
+//         console.log(`You are ${arg1}, ${arg3}`);
+//     } else {
+//         console.log('false');
+//     }
+// }
+// thisIsTrue('nice', 'Nice', 'test');
+
+
+const canDrive = (age) => {
+    if (age >= 15) {
+        return true;
+    }
+    return false; // it will return undefined if you don't return the false
+}
+console.log(canDrive(13) ? 'you can drive' : 'you are too young');
+
+
+
+
+// END
