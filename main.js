2ddcdb90df93f7a97aeb43e0f8b4f908add0afec
JuanDavila1101 <tatojuan713@hotmail.com>
2021-01-16 11:08:48 -0600

    did varialbe, .math library, pow, ceil, round

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log("js-intro for JID ");
+// console.log("js-intro for JID ");
+// console.log("CONNECTED");
+// let firstName = "Beyonce";
+// let lastName = "Knowels";
+// console.log(firstName + lastName);
+// console.log(firstName + " " + lastName);
+// // The back ticks are called template literal ``
+// console.log(`First name:${firstName}\nLast name:${lastName}`);
+// firstName = 'Juan';
+// lastName = 'Davila';
+// console.log(`First name:${firstName}\nLast name:${lastName}`);
+// const favorateFood = 'Tacos';
+// console.log(favorateFood);
+// favorateFood = 'Gumbo';
+// console.log(favorateFood);
+
+// const dog = 'bark';
+// const answer = 33;
+// const negative = -14;
+// const decimal = 5.5;
+// const myFlag = true;
+// console.log(typeof decimal, typeof myFlag);
+
+// let myCohersion = '1'+2 // = 12 
+
+//exponents Math.pow(2,2)
+
+// console.log(Math.pow(2, 2));
+// console.log(Math.pow(3, 2));
+// console.log(Math.pow(3, 3));
+
+// console.log(Math.round(6.5)); //7
+// console.log(Math.round(6.45)); //6
+// console.log(Math.floor(6.5)); //6
+// console.log(Math.ceil(6.4)); //7
+
+const cat = 'Kitty'
+
+console.log(cat.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//END
